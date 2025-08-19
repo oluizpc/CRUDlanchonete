e43e64ad7623dd7589ced3ca3e44727e1749574d
Luiz Paullo <104634868+oluizpc@users.noreply.github.com>
2025-08-19 12:50:47 -0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # A string de conexão correta para o seu projeto no Supabase
-DATABASE_URL = "postgresql://postgres.azzdebrqffqsmsxgxylp:luizpaullo12345@aws-1-sa-east-1.pooler.supabase.com:6543/espetinholuiz"
+
 # Cria o "motor" do SQLAlchemy com a sua DATABASE_URL
 engine = create_engine(DATABASE_URL)
 
